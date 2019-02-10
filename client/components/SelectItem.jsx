f08355362ddd4217270afaed3b4b34ed0477cce0
ranjit03 <ranjitendurthi03@gmail.com>
2019-02-10 14:22:52 +1300

    state applied to stock on hand function

--- client/components/SelectItem.jsx
+++ client/components/SelectItem.jsx
@@ -3,16 +3,24 @@ import AddItem from "./AddItem";
 import { getItemDetails } from "../api/index";
 
 class SelectItem extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: null
+    };
     this.stockOnHand = this.stockOnHand.bind(this);
     this.addStock = this.addStock.bind(this);
     this.deleteStock = this.deleteStock.bind(this);
+    this.buttonValueUpdate = this.buttonValueUpdate.bind(this);
   }
 
   stockOnHand() {
     getItemDetails(event.target.value).then(stock => (
-      <div>{window.alert("Stock on hand is " + stock[0].stock_on_hand)}</div>
+      <div>
+        {window.alert(
+          stock[0].name + " Stock on hand is " + stock[0].stock_on_hand
+        )}
+      </div>
     ));
   }
   addStock() {
@@ -23,16 +31,20 @@ class SelectItem extends React.Component {
     return <div>{prompt("Please enter quantity", "Example: 50")}</div>;
   }
 
+  buttonValueUpdate(e) {
+    return this.setState({ item: e.target.value });
+  }
+
   render() {
     return (
       <div className="row">
         <div className="col-6">
-          <select className="margin">
-            <option value="beer mug">Beer Mugs</option>
-            <option value="champagne flutes">Champagne Flutes</option>
-            <option value="red wine glasses">Red Wine Glasses</option>
+          <select className="margin" onChange={this.buttonValueUpdate}>
+            <option value="test 1">Beer Mugs</option>
+            <option value="test 2">Champagne Flutes</option>
+            <option value="test 3">Red Wine Glasses</option>
           </select>
-          <button value="test 3" onClick={this.stockOnHand}>
+          <button value={this.state.item} onClick={this.stockOnHand}>
             Stock on hand
           </button>
           <br />
